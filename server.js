af8916d7e284a7e5af41a709b754ef753312c528
Sara Fotros <sarafotros@gmail.com>
2020-08-21 10:12:44 +0100

    react2

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-app.use(express.static(path.join(__dirname, './public')))
+app.use(express.static(path.join(__dirname, './client/build/')))
 
 app.get('/listall', (req, res)=>{
     // console.log('this route was hit')
